484a5c519dd6b547563995766b62db9c591ae645
Shubham Jadhav <111007875+ShubhamJadhav-github@users.noreply.github.com>
2023-06-30 14:26:48 +0530

    Create Average.java
    //Program to calculate avarage of two numbers

---
+++ Average.java
@@ -0,0 +1,11 @@
+//Program to calculate avarage of two numbers
+class Average
+{
+	public static void main(String args[])
+	{
+		int a=10,b=22,c=12;
+		float avg;
+		avg = (a+b+c)/3.0f;
+		System.out.print("\nAverage = "+avg);
+	}
+}	
